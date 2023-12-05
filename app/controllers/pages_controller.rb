13aecbb4c2605efb4a2b083331bf81a5614d5821
Evhiro <playingfrizz@gmail.cm>
2023-12-05 16:10:37 +0800

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,64 @@
+class PagesController < ApplicationController
+    def landing
+        render "pages/_landing"
+    end
+
+    #ADMIN PAGES
+    def admin
+        render "pages/Admin/_login"
+    end
+    def student_table
+        render "pages/Admin/_tablestudent"
+    end
+    def teacher_table
+        render "pages/Admin/_tableteacher"
+    end
+    def admin_accounts
+        render "pages/Admin/_accounts"
+    end
+    def admin_settings
+        render "pages/Admin/_settings"
+    end
+
+    #STUDENT PAGES
+    def student
+        render "pages/Student/_login"
+    end
+
+    #TEACHER PAGES
+    def teacher
+        render "pages/Teacher/_login"
+    end  
+
+        #LOGIN COMMANDS
+        def new
+        
+        end
+    
+        def create
+            user = Login.find_by(email: params[:email])
+        
+            if user && user.authenticate(params[:password]) && user.account_type == params[:account_type]
+              session[:user_id] = user.id
+        
+              case params[:account_type]
+              when "admin"
+                redirect_to student_table_path
+              when "student"
+                redirect_to student_dashboard_path
+              when "teacher"
+                redirect_to teacher_dashboard_path
+              else
+                redirect_to root_path
+              end
+            else
+              flash.now[:alert] = "Invalid email or password"
+              redirect_to root_path
+            end
+          end
+    
+        def destroy
+            session[:user_id] = nil
+            redirect_to root_path, notice: 'Logged out successfully!'
+        end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  root "pages#landing"
+
+  get "/admin" => "pages#admin", as: "admin_login", account_type: "admin" 
+  get "/student" => "pages#student", as: "student_login", account_type: "student" 
+  get "/teacher" => "pages#teacher", as: "teacher_login", account_type: "teacher" 
+
+  #ADMIN
+  get "/admin/student" => "pages#student_table", as: "student_table"
+  get "/admin/teacher" => "pages#teacher_table", as: "teacher_table"
+  get "/admin/accounts" => "pages#admin_accounts", as: "admin_accounts"
+  get "/admin/settings" => "pages#admin_settings", as: "admin_settings"
+
+  post "/student/log", to: "pages#create", as:"student_log", account_type: "student"
+  post "/teacher/log", to: "pages#create", as:"teacher_log", account_type: "teacher"
+  post "/admin/student/log", to: "pages#create", as:"admin_log", account_type: "admin" 
+
+
+end
